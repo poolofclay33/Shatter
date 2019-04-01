cec6ea6a9a5b4163b05f5e53b62b0f0265740b4f
poolofclay33 <39412974+poolofclay33@users.noreply.github.com>
2019-04-01 15:46:11 -0500

    Base

---
+++ (Enter Title)/Assets/Scripts/Jump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [Range(1, 10)]
+    public float jumpVelocity;
+
+    public bool grounded = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") && grounded == true)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.up * jumpVelocity;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            grounded = false;
+        }
+    }
+}
---
+++ (Enter Title)/Assets/Scripts/Movement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public int speed;
+    public float fallMulti = 2.5f;
+    public float lowJump = 2f;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //movement 
+        transform.Translate(Time.deltaTime * speed, 0, 0);
+
+        Vector3 pos = transform.position;
+
+        pos.z = Mathf.Clamp(transform.position.z, -1, 1);
+
+        transform.position = pos;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if(transform.position.z == 1)
+            {
+                Debug.Log("NOTHING");
+            } 
+            else 
+            {
+                transform.Translate(0f, 0f, 1f);
+                Mathf.RoundToInt(transform.position.z);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (transform.position.z == -1)
+            {
+                Debug.Log("NOTHING MORE");
+            }
+            else
+            {
+                transform.Translate(0f, 0f, -1f);
+            }
+        }
+
+        //jump
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMulti - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJump - 1) * Time.deltaTime;
+        }
+    }
+}
